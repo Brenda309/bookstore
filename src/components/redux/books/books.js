9ed916bac20c0f37c25dba8360a293e0e28760d1
Brenda309 <brewihogora23@gmail.com>
2022-07-15 17:06:51 +0200

    Add reducers for add and remove a book

--- src/components/redux/books/books.js
+++ src/components/redux/books/books.js
@@ -1,2 +1,35 @@
 const BOOK_ADDED = ' BOOK_ADDED';
 const BOOK_REMOVED = ' BOOK_REMOVED';
+
+export function addBook() {
+  return {
+    type: BOOK_ADDED,
+  };
+}
+export function removeBook() {
+  return {
+    type: BOOK_REMOVED,
+  };
+}
+const initialState = {
+  books: [],
+  newBook: '',
+};
+
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    case BOOK_ADDED:
+      return {
+        ...state,
+        newBook: '',
+      };
+
+    case BOOK_REMOVED:
+      return {
+        ...state,
+        newBook: '',
+      };
+    default:
+      return state;
+  }
+}
